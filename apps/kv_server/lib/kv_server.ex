12f9bb36dbb3dec06c3f1234b5e148b267dab312
Florian Kempenich <shockn745@gmail.com>
2017-01-28 18:55:05 +0000

    Implement basic tcp echo server

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,13 +1,49 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for KVServer.
-  """
+  require Logger
 
-  def hello(bucket_name, to_save) do
-    KV.Registry.create(KV.Registry, bucket_name)
-    {:ok, bucket} = KV.Registry.lookup(KV.Registry, bucket_name)
+  def accept(port) do
+    # The options below mean:
+    #
+    # 1. ':binary' - receives data as binaries (instead of lists)
+    # 2. 'packet: :line' - receives data line by line
+    # 3. 'active: false' - blocks on ':gen_tcp.recv/2' until data is available
+    # 4. 'reuseaddr: true' - allows us to reuse the address if the listener crashes
+    #
 
-    KV.Bucket.put(bucket, to_save, 23)
-    bucket
+    {:ok, socket} = :gen_tcp.listen(
+      port,
+      [:binary, packet: :line, active: false, reuseaddr: true]
+    )
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket)
   end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(
+      KVServer.TaskSupervisor,
+      fn -> serve(client) end
+    )
+    # Delegate the resposability of the socket to the process with 'pid'
+    # Otherwise the process that accepted (this one) would be responsible ... and if it crashes ... all socket crashjkk
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    Logger.info "Reading line: #{data}"
+    data
+  end
+
+  defp write_line(line, socket), do: :gen_tcp.send(socket, line)
+
 end
--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -1,6 +1,4 @@
 defmodule KVServer.Application do
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -12,10 +10,10 @@ defmodule KVServer.Application do
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg1, arg2, arg3)
       # worker(KVServer.Worker, [arg1, arg2, arg3]),
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [1234]])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
