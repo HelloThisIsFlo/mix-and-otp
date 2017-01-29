2d9b08499c18c8bb7140604a2c5fcacdd67ad803
Florian Kempenich <shockn745@gmail.com>
2017-01-29 14:37:28 +0000

    Use different ports for tests

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -6,16 +6,25 @@ defmodule KVServer.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg1, arg2, arg3)
       # worker(KVServer.Worker, [arg1, arg2, arg3]),
       supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
-      worker(Task, [KVServer, :accept, [1234]]),
+      worker(Task, [KVServer, :accept, [port()]]),
       supervisor(Task.Supervisor, [[name: KVServer.RouterTasks]], id: KVServer.RouterTasks)
     ]
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp port do
+    if Mix.env == :test do
+      1234
+    else
+      4040
+    end
   end
 end
